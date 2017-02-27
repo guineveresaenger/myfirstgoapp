a1662bcc978943014e0180567f4a8383367c5d4b
guineveresaenger <guineveresaenger@gmail.com>
2017-02-27 14:30:30 -0800

    renamed some files and got fields to show in favoriteColor page

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,19 +8,27 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Person struct {
-	ID        string   `json:"id,omitempty"`
-	Firstname string   `json:"firstname,omitempty"`
-	Lastname  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
+// type Person struct {
+// 	ID        string   `json:"id,omitempty"`
+// 	Firstname string   `json:"firstname,omitempty"`
+// 	Lastname  string   `json:"lastname,omitempty"`
+// 	Address   *Address `json:"address,omitempty"`
+// }
+//
+// type Address struct {
+// 	City  string `json:"city,omitempty"`
+// 	State string `json:"state,omitempty"`
+// }
+//
+// var people []Person
+
+type Color struct {
+	ID   string
+	Name string
 }
 
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-}
-
-var people []Person
+var colors []Color
+var green = Color{ID: "4", Name: "green"}
 
 func index(w http.ResponseWriter, req *http.Request) {
 	t, err := template.ParseFiles("./templates/index.html")
@@ -32,65 +39,65 @@ func index(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func colors(w http.ResponseWriter, req *http.Request) {
+func favoriteColor(w http.ResponseWriter, req *http.Request) {
 	name := req.URL.Query().Get("name")
 	log.Println(name)
-
-	// s := `./templates/colors.html`
-	// t := template.Must(template.New("test").Parse(s))
-	t, _ := template.ParseFiles("./templates/colors.html")
-	t.Execute(w, name)
+	t, _ := template.ParseFiles("./templates/favoriteColor.html")
+	t.Execute(w, green)
 }
 
-func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	for _, item := range people {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-
-	}
-	json.NewEncoder(w).Encode(&Person{})
-}
-
-func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
-	json.NewEncoder(w).Encode(people)
-}
-
-func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	var person Person
-	_ = json.NewDecoder(req.Body).Decode(&person) // ?? what does this line do exactly?
-	person.ID = params["id"]
-	people = append(people, person)
-	json.NewEncoder(w).Encode(people)
-}
-
-func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	for index, item := range people {
-		if item.ID == params["id"] {
-			// the following line slices right before current index, and right after, skipping the current index.
-			// Read up on the ... in Go.
-			people = append(people[:index], people[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(people)
-}
+// func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+// 	params := mux.Vars(req)
+// 	for _, item := range people {
+// 		if item.ID == params["id"] {
+// 			json.NewEncoder(w).Encode(item)
+// 			return
+// 		}
+//
+// 	}
+// 	json.NewEncoder(w).Encode(&Person{})
+// }
+//
+// func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
+// 	json.NewEncoder(w).Encode(people)
+// }
+//
+// func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+// 	params := mux.Vars(req)
+// 	var person Person
+// 	_ = json.NewDecoder(req.Body).Decode(&person) // ?? what does this line do exactly?
+// 	person.ID = params["id"]
+// 	people = append(people, person)
+// 	json.NewEncoder(w).Encode(people)
+// }
+//
+// func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+// 	params := mux.Vars(req)
+// 	for index, item := range people {
+// 		if item.ID == params["id"] {
+// 			// the following line slices right before current index, and right after, skipping the current index.
+// 			// Read up on the ... in Go.
+// 			people = append(people[:index], people[index+1:]...)
+// 			break
+// 		}
+// 	}
+// 	json.NewEncoder(w).Encode(people)
+// }
 
 func main() {
 	router := mux.NewRouter()
-	people = append(people, Person{ID: "1", Firstname: "Guin", Lastname: "Awesome", Address: &Address{City: "Seattle", State: "WA"}})
-	people = append(people, Person{ID: "2", Firstname: "Brendan", Lastname: "Batman"})
+	colors = append(colors, Color{ID: "1", Name: "blue"})
+	colors = append(colors, Color{ID: "1", Name: "red"})
+	colors = append(colors, Color{ID: "1", Name: "yellow"})
+	// people = append(people, Person{ID: "1", Firstname: "Guin", Lastname: "Awesome", Address: &Address{City: "Seattle", State: "WA"}})
+	// people = append(people, Person{ID: "2", Firstname: "Brendan", Lastname: "Batman"})
 
-	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
+	// router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	// router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
+	// router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
+	// router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
 	router.HandleFunc("/", index).Methods("GET")
-	router.HandleFunc("/colors", colors).Methods("GET")
+	router.HandleFunc("/favoriteColor", favoriteColor).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":12345", router))
 
