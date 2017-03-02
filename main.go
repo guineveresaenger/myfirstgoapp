8db242fa5d6d3bb2af2760ea133e4cb41216547d
guineveresaenger <guineveresaenger@gmail.com>
2017-03-02 10:28:08 -0800

    added database package and driver to main, and called on colors from database. Success

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gorilla/mux"
 )
@@ -122,10 +125,41 @@ func addNewColor(w http.ResponseWriter, req *http.Request) {
 func main() {
 
 	log.Println("main is running")
+
+	//get database
+	db, err := sql.Open("mysql", "root@tcp(a3d1318d1fe4111e6a2240a13f4ea03d-294149056.us-west-2.elb.amazonaws.com:3306)/myfirstgoapp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// get rows
+	rows, err := db.Query("SELECT * FROM colors")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var color = Color{}
+		err := rows.Scan(&color.ID, &color.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		colors = append(colors, color)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, color := range colors {
+		log.Println(color.Name)
+	}
+
 	router := mux.NewRouter()
-	colors = append(colors, Color{ID: 1, Name: "blue"})
-	colors = append(colors, Color{ID: 2, Name: "red"})
-	colors = append(colors, Color{ID: 3, Name: "yellow"})
+	// colors = append(colors, Color{ID: 1, Name: "blue"})
+	// colors = append(colors, Color{ID: 2, Name: "red"})
+	// colors = append(colors, Color{ID: 3, Name: "yellow"})
 	// people = append(people, Person{ID: "1", Firstname: "Guin", Lastname: "Awesome", Address: &Address{City: "Seattle", State: "WA"}})
 	// people = append(people, Person{ID: "2", Firstname: "Brendan", Lastname: "Batman"})
 
